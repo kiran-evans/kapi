7bd4e6716e63811a4f3c3184540d79102a707950
kiran-evans <thekiranevans@gmail.com>
2023-08-01 23:55:31 +0100

    Added basic structure for api and added first route/controller pair

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
-import cors, { CorsOptions } from 'cors';
-import dotenv from 'dotenv';
+// App setup and config
+
 import express from 'express';
-
-dotenv.config();
-
 const app = express();
 app.use(express.json());
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+import cors, { CorsOptions } from 'cors';
 const corsOptions: CorsOptions = {
     allowedHeaders: ['Content-Type']
 };
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+
+// Routes
+
+import productRouter from './routers/productRouter';
+app.use(productRouter);
+
+import userRouter from './routers/userRouter';
+app.use(userRouter);
+
+import cartRouter from './routers/cartRouter';
+app.use(cartRouter);
+
+import orderRouter from './routers/orderRouter';
+app.use(orderRouter);
+
+// Server start
 
 const port = process.env.PORT;
 app.listen(port, () => {
