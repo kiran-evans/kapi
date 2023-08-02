0b1378d71505960fad14b43911a0aebd6ef6d9c9
kiran-evans <thekiranevans@gmail.com>
2023-08-02 10:24:39 +0100

    Added get all route

--- src/controllers/Product.ts
+++ src/controllers/Product.ts
@@ -3,11 +3,25 @@ import { pool } from "../pg";
 
 export const GET = (async (req, res) => {
     try {
-        const { rows, rowCount } = await pool.query(`SELECT * FROM products WHERE id = ${req.query.id}`);
+        const { rows, rowCount } = await pool.query(`SELECT * FROM products WHERE id = ${req.params.id}`);
 
         if (!rowCount) res.status(404).send();
 
         res.status(200).json(rows[0]);
+
+    } catch (err: any) {
+        console.error(err);
+        res.status(500).send();
+    }
+}) satisfies RequestHandler;
+
+export const GET_ALL = (async (req, res) => {
+    try {
+        const { rows, rowCount } = await pool.query(`SELECT * FROM products`);
+
+        if (!rowCount) res.status(404).send();
+
+        res.status(200).json(rows);
 
     } catch (err: any) {
         console.error(err);
--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { GET } from "../controllers/Product";
 
 const router = Router();
-const route = router.route('/product');
+const route = router.route('/product/:id');
+const allRoute = router.route('/product/all');
 
 route.get(GET);
 
