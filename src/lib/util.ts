604e3e43e100b47127c6be0082c18bf21ec7198e
kiran-evans <thekiranevans@gmail.com>
2023-08-29 15:18:49 +0100

    Updated consolidateCarts() to update duplicate CartItems rather than add new ones to the db

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -15,51 +15,63 @@ export const consolidateCarts = async (pgCartItemIds: Array<string>, reqCartItem
         If two items have the exact same product_id, colour and size, then consolidate them into one item with the quantity
             value being the sum of their quantities.
     */
-    const newCartItems = Array<any>();
-    const dbCartItems = Array<any>();
-    const clientCartItems = [...reqCartItems];
+    // This array will be systematically shrunk so that only new items remain
+    const remainingCartItems = [...reqCartItems];
+
+    // This array will serve as the list of CartItems that already exists the the db
+    const existingCartItemsInDb = Array<CartItem>();
     
-    // Get the data for each of the db cartItems
+    // This array serves as the result of adding existing cart items after they have been updated, and finally adding the remainingCartItems
+    const consolidatedCartItems = Array<CartItem>();
+    
+    // Get the data for each of the CartItems that already exist in the db
     for (const cartItemId of pgCartItemIds) {
         const cartItem = await CartItem.findByPk(cartItemId);
-        dbCartItems.push(cartItem);
+        if (!cartItem) throw `No CartItem found with id=${cartItemId}`;
+        existingCartItemsInDb.push(cartItem);
     }
 
     // Consolidate duplicate items (ones that have the same product_id and colour and size options)
-    for (let i = 0; i < dbCartItems.length; i++) {
-        for (let j = 0; i < clientCartItems.length; j++) {
+    for (let i = 0; i < existingCartItemsInDb.length; i++) {
+        for (let j = 0; i < remainingCartItems.length; j++) {
             // Check if they are a match, in which case consolidate them
             if (
-                (dbCartItems[i].product_id === clientCartItems[j].product_id)
-                && (dbCartItems[i].colour === clientCartItems[j].colour)
-                && (dbCartItems[i].size === clientCartItems[j].size)
+                (existingCartItemsInDb[i].product_id === remainingCartItems[j].product_id)
+                && (existingCartItemsInDb[i].colour === remainingCartItems[j].colour)
+                && (existingCartItemsInDb[i].size === remainingCartItems[j].size)
             ) {
-                newCartItems.push({
-                    ...dbCartItems[i],
-                    quantity: dbCartItems[i].quantity + clientCartItems[j].quantity
+                // Increase the quantity of the item in the database
+                const [affectedCount, affectedRows] = await CartItem.update({
+                    quantity: existingCartItemsInDb[i].quantity + remainingCartItems[j].quantity
+                }, {
+                    where: {
+                        id: existingCartItemsInDb[i].id
+                    },
+                    returning: true
                 });
-                dbCartItems.splice(i, 1);
-                dbCartItems.splice(j, 1);
+                // Remove the item at this index from the array of "new" items because it already exists and has now had its quantity updated
+                remainingCartItems.splice(i, 1);
+                // Add this item's updated body to the final output
+                consolidatedCartItems.push(affectedRows[0]);
                 continue;
             }
         }
     }
 
-    // Create new cart_items in the db for the reamining clientCartItems
-    for (const clientCartItem of clientCartItems) {
-        clientCartItem.id = await addNewCartItemToDb(clientCartItem);
+    // Create new cart_items in the db for the remaining items which WERE NOT duplicates
+    for (const remainingItem of remainingCartItems) {
+        consolidatedCartItems.push(await addNewCartItemToDb(remainingItem));
     }
 
-    // Add in the remaining items
-    return newCartItems.concat(dbCartItems).concat(clientCartItems);
+    return consolidatedCartItems;
 }
 
-export const addNewCartItemToDb = async (cartItem: CartItem): Promise<string> => {
+export const addNewCartItemToDb = async (cartItem: CartItem): Promise<CartItem> => {
     const newCartItem = await CartItem.create({
         product_id: cartItem.product_id,
         quantity: 1,
         colour: cartItem.colour,
         size: cartItem.size
     });
-    return newCartItem.id;
+    return newCartItem;
 }
