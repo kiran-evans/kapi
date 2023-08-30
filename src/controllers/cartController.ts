c66966964490201fdf6cfbf108d0a897a66e4362
kiran-evans <thekiranevans@gmail.com>
2023-08-30 12:26:49 +0100

    Modified handling of cart api to make auth flow simpler

--- src/controllers/cartController.ts
+++ src/controllers/cartController.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import { fb } from "../firebase";
 import { addToCart } from "../lib/util";
 import { CartItem } from "../models/CartItem";
+import { Product } from "../models/Product";
 import { User } from "../models/User";
 
 // Combine carts of client and db (for when a user logs in on the client)
@@ -10,13 +11,17 @@ export const REPLACE_CART = (async (req, res) => {
         // Verify encoded id token passed from client (checks user has been created and signed in on the client side)
         const idToken = await fb.auth().verifyIdToken(req.params.idToken);
         // Get the user data from the db
-        const user = await User.findOne({
+        const [user, created] = await User.findOrCreate({
             where: {
+                auth_id: idToken.uid
+            },
+            defaults: {
                 auth_id: idToken.uid
             }
         });
 
-        if (!user) return res.status(404).send();
+        if (created) console.log(`[server] new user created because none was found`);        
+        if (!user) throw `Failed to find or create user with auth_id=${idToken.uid}`;
         
         const newCartItemIds = Array<string>();
         req.body.items.forEach((cartItem: CartItem) => {
@@ -53,7 +58,7 @@ export const ADD_TO_CART = (async (req, res) => {
             }
         });
 
-        if (!user) return res.status(404).json(`No user found with auth_id=${idToken.id}`);
+        if (!user) return res.status(404).json(`No user found with auth_id=${idToken.uid}`);
 
         // Combine the existing cart in the db with the item in the request, handling duplicates where necessary
         const newCartItemIds = await addToCart(user.cart_item_ids, req.body.item);
@@ -68,6 +73,27 @@ export const ADD_TO_CART = (async (req, res) => {
 
         // Send the updated list back to the client
         res.status(200).send(newCartItemIds);
+
+    } catch (err: any) {
+        console.error(err);
+        res.status(500).send();
+    }
+}) satisfies RequestHandler;
+
+// Get cart item
+export const GET = (async (req, res) => {
+    try {
+        // Get a cart item and return it with its related product data
+        const cartItem = await CartItem.findByPk(req.params.id);
+        if (!cartItem) return res.status(404).send();
+
+        const product = await Product.findByPk(cartItem.product_id);
+        if (!product) return res.status(404).send();
+
+        res.status(200).json({
+            cartItemData: await cartItem.toJSON(),
+            productData: await product.toJSON()
+        });
 
     } catch (err: any) {
         console.error(err);
