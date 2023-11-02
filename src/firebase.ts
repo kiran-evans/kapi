36def90167fc190d1506add09dce166a1a52e0c3
kiran-evans <thekiranevans@gmail.com>
2023-11-02 16:00:24 +0000

    Escaped private key characters

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,7 +3,7 @@ import firebase from 'firebase-admin';
 export const fb = firebase.initializeApp({
     credential: firebase.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL
     })
 });
