d8ac2f463e13233861d0bd0f5728d8786fb6d101
kiran-evans <thekiranevans@gmail.com>
2023-11-05 23:23:08 +0000

    Setup esbuild

--- dist/pg.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelize = void 0;
-const sequelize_1 = require("sequelize");
-exports.sequelize = new sequelize_1.Sequelize(process.env.DB_NAME ?? '', process.env.DB_USERNAME ?? '', process.env.DB_PW, {
-    host: String(process.env.DB_HOST),
-    port: Number(process.env.DB_PORT),
-    dialect: 'postgres'
-});
